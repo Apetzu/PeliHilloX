529ced7ec72ee014c7f19582b5cc75c561f5c651
Aapo Ritvanen <aapo.ritvanen@gmail.com>
2018-01-26 21:57:31 +0200

    Simple player movement

---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    [SerializeField]
+    float acceleration = 10f;
+    [SerializeField]
+    float maxSpeed = 50f;
+
+    Rigidbody rb;
+    Vector3 movDir = Vector3.zero;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+    {
+        movDir = new Vector3(Input.GetAxisRaw("XMovement"), 0, Input.GetAxisRaw("ZMovement")).normalized;
+        rb.AddForce(movDir * acceleration * rb.mass);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+	}
+}
