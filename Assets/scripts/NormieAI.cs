ccf7c37ecffd003a65bc001edeed4698a6130441
Dreadfang <jannekarhu2@kao.fi>
2018-01-27 19:53:56 +0200

    Spawners and hopping :mans_shoe:

--- Assets/scripts/NormieAI.cs
+++ Assets/scripts/NormieAI.cs
@@ -25,10 +25,13 @@ public class NormieAI : MonoBehaviour {
     float starthealth;
 
 	Collider[] InfectedDetected;
+    public Animator anime;
+    public float idleLimit;
 
 	void Start () 
 	{
         starthealth = health;
+        wanderTimer = Random.Range(1.5f,3f);
 		agent = GetComponent<NavMeshAgent> ();
 		timer = wanderTimer;
 	}
@@ -36,13 +39,15 @@ public class NormieAI : MonoBehaviour {
 	void Update () 
 	{
 		InfectedDetected = Physics.OverlapSphere (transform.position, DetectorRadius, ~(1 << LayerMask.NameToLayer ("Normies") | 1));
-
+ 
 		if (InfectedDetected.Length > 0) 
 		{
+            
             Flee();
 		}
         else 
 		{
+            
             timer += Time.deltaTime;
 
             if (timer >= wanderTimer) {
@@ -51,10 +56,17 @@ public class NormieAI : MonoBehaviour {
                 timer = 0;
             }
 		}
+        if(agent.velocity.magnitude < idleLimit)
+        {
+            anime.SetInteger("AnimPos",0);
+        }
+        else
+            anime.SetInteger("AnimPos",1);
 	}
 
     void Flee()
     {
+        anime.SetInteger("AnimPos",2);
         Vector3 FleeDirection = -(InfectedDetected[0].transform.position - transform.position);
         transform.rotation = Quaternion.LookRotation(new Vector3(FleeDirection.x, 0, FleeDirection.z));
         Vector3 FleeTo = transform.position + transform.forward * multiplyBy;
---
+++ Assets/scripts/SpawnerNormie.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpawnerNormie : MonoBehaviour 
+{
+    private NavMeshAgent agent;
+    public float spawnLimit;
+    float spawnedAmount;
+    public GameObject Normie;
+    public float spawnRadius;
+	void Start () 
+    {
+        agent = GetComponent<NavMeshAgent> ();
+
+        for (int i = 0; i < spawnLimit; i++)
+        {
+            Vector3 newPos = RandomNavSphere (transform.position, spawnRadius, -1);
+            Instantiate(Normie, newPos, Normie.transform.rotation);
+        }
+	}
+        
+    Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask) 
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+        randDirection += origin;
+        NavMeshHit navHit;
+        NavMesh.SamplePosition (randDirection, out navHit, dist, layermask);
+        return navHit.position;
+    }
+}
