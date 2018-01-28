dc33093c5785356a5031ac08cd54f7c6a70d47f3
Dreadfang <jannekarhu2@kao.fi>
2018-01-28 15:10:34 +0200

    Infected homing and score and icons

--- Assets/scripts/SpawnerNormie.cs
+++ Assets/scripts/SpawnerNormie.cs
@@ -17,8 +17,12 @@ public class SpawnerNormie : MonoBehaviour
     [SerializeField]
     float navMeshSearchDist = 10;
     public Text PeopleCount;
+    public GameObject PeoplePanel;
+    public GameObject ScorePanel;
+    public Text ScoreText;
     int InfectedAmount;
     int NormieAmount;
+    public int scoreGain = 5;
 
     void Start () 
     {
@@ -37,7 +41,17 @@ public class SpawnerNormie : MonoBehaviour
     {
         InfectedAmount = GameObject.FindGameObjectsWithTag("Infected").Length;
         NormieAmount = GameObject.FindGameObjectsWithTag("Normie").Length;
-        PeopleCount.text = "Infected: " + InfectedAmount + "     Normies: "+ NormieAmount;
+        PeopleCount.text = "X:" + NormieAmount + "\n\nX:" + InfectedAmount + "\n\nX:" + (spawnLimit - InfectedAmount - NormieAmount);
+        if(NormieAmount == 0)
+        {
+            PeoplePanel.SetActive(false);
+            ScorePanel.SetActive(true) ;
+            ScoreText.text = "Your Score: " + (InfectedAmount * scoreGain);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 
     Vector3 RandomNavPos(float xSize, float zSize, float dist, int layermask)
